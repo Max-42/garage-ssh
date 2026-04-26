649102d1ce6aeb515e658c40f226bb8a32ef56c0
max <max@dyren.de>
2026-04-26 11:22:53 +0200

    fix: build arch-correct addon image; ci: automatic versioning

--- garage_ssh_gate/run.sh
+++ garage_ssh_gate/run.sh
@@ -8,6 +8,8 @@ LOG_LEVEL=$(bashio::config 'log_level')
 
 bashio::log.info "SSH Port: ${SSH_PORT}"
 bashio::log.info "Log Level: ${LOG_LEVEL}"
+bashio::log.info "Container Arch: $(uname -m)"
+bashio::log.info "App Version: ${APP_VERSION:-unknown}"
 
 # Ensure data directory exists
 mkdir -p /data
